2bacc22c1c6adc237cfd4c5d79d46fee62f6cf61
rafailbala <balaevrafail@gmail.com>
2022-11-26 04:16:18 +0400

    T4-5 adding request: task accomplished

--- src/main/java/com/example/homeownersspring/service/impl/RequestService.java
+++ src/main/java/com/example/homeownersspring/service/impl/RequestService.java
@@ -1,5 +1,6 @@
 package com.example.homeownersspring.service.impl;
 
+import com.example.homeownersspring.model.Request;
 import com.example.homeownersspring.repo.RequestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,8 @@ public class RequestService {
 
     @Autowired
     private RequestRepository requestRepository;
+
+    public void save(Request request){
+        requestRepository.save(request);
+    }
 }
