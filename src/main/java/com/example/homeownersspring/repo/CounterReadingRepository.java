b8f7154870776f92d4dc2ac6d5c195fdaf66d949
Храмов Никита Владимирович <nick.khramov@yandex.ru>
2023-01-05 11:46:59 +0400

    Revert "Т4-6-adding_indication_history: adding rest for get counter reading history"
    This reverts commit 8cbdac0401a86d3614abd1ff76028746247cefc7.

--- src/main/java/com/example/homeownersspring/repo/CounterReadingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.homeownersspring.repo;
-
-import com.example.homeownersspring.model.CounterReading;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CounterReadingRepository extends JpaRepository<CounterReading, Long> {
-}
